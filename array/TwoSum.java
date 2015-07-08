ab4ab9bbd4efd091a23cddb83a9b4640d5b35d8b
wayetan <tanw@usc.edu>
2015-07-07 23:36:12 -0700

    save workspace

---
+++ array/TwoSum.java
@@ -0,0 +1,17 @@
+import java.lang.Integer;
+import java.util.HashMap;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
